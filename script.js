b28af24e3fbbda902f3ba52e618fd66a5cfa1daf
Prathmesh kolam <prathmeshkolam@gmail.com>
2026-05-25 15:41:34 +0530

    "Added Section-2"

--- script.js
+++ script.js
@@ -4,3 +4,46 @@
   btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
   });
+
+  const cards = document.querySelector(".cards");
+
+document.querySelector(".right").onclick = () => {
+  cards.scrollBy({ left: 400, behavior: "smooth" });
+};
+
+document.querySelector(".left").onclick = () => {
+  cards.scrollBy({ left: -400, behavior: "smooth" });
+};
+
+const slider = document.querySelector(".cards");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+
+  e.preventDefault();
+
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 0.8;
+
+  slider.scrollLeft = scrollLeft - walk;
+});
